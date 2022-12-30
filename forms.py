209da226d258f62a8ce62906c41d7b23143aac01
michaelpappas <pappas.michael@gmail.com>
2022-12-30 08:41:20 -0800

    initial cafe add/edit form

--- forms.py
+++ forms.py
@@ -1,1 +1,43 @@
 """Forms for Flask Cafe."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextField, SelectField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Length, URL
+
+class CafeForm(FlaskForm):
+    """ Form for adding a cafe. """
+
+    name = StringField(
+        "Name",
+        validators=[
+            InputRequired(),
+            Length(max=30)
+        ]
+    )
+
+    description = TextField(
+        "Description",
+    )
+
+    url = StringField(
+        "URL",
+        validators=[URL()]
+    )
+
+    address = StringField(
+        "Address",
+        validators=[
+            InputRequired(),
+            Length(max=50)
+        ]
+    )
+
+    city = SelectField(
+        "city",
+        coerce=int)
+
+    image = StringField(
+        "Image Url",
+        validators=[URL()]
+    )
+
